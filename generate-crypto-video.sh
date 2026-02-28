081838047ae4f55360a6d493d8127c6ab4f3eed4
Stiuca <stiuca@email.com>
2026-02-28 17:12:38 +0000

    Generate 15-sec crypto video using evolink

---
+++ generate-crypto-video.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Generate crypto video task for Sasha
+
+echo "Generating 15-second crypto video using evolink API..."
+
+# This file triggers Sasha to generate the video
+# Sasha will use the evolink-video skill to create the video
+
+TASK="Create a 15-second cinematic video about crypto using evolink-video skill. Save to /home/user/Videos/crypto_video.mp4"
